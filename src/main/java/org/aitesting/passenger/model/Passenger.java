e8fb065f73971c11fa9d923006b4e137012faa35
MarvinAmari <contact@marvinamari.com>
2018-04-20 19:17:34 -0400

    Remove comments and unnessary imports

--- src/main/java/org/aitesting/passenger/model/Passenger.java
+++ src/main/java/org/aitesting/passenger/model/Passenger.java
@@ -1,12 +1,9 @@
 package org.aitesting.passenger.model;
 
+
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.NotEmpty;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,18 +17,6 @@ public class Passenger {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    // passwords will be stored elsewhere
-    // no CQRS
-    // use command pattern
-    @NotBlank
-    private String userName;
-
-    @NotBlank
-    private String passWord;
-
-    @NotBlank
-    private String confPassword;
-
     @NotBlank
     private String firstName;
 
@@ -44,7 +29,5 @@ public class Passenger {
 
     @NotBlank
     private int phoneNumber;
-
-
 
 }
