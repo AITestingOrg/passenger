6ea162cc12869f93828402fccc386b6eba2e710c
MarvinAmari <contact@marvinamari.com>
2018-04-21 22:39:49 -0400

    Initial state where application compiles and is usable

--- out/production/resources/db/changelog/changes/v0001.sql
+++ out/production/resources/db/changelog/changes/v0001.sql
@@ -10,16 +10,16 @@
    primary key (id)
  );
 
- create table ride (
-   id bigserial not null,
-   start_addr varchar(20) not null,
-   start_city varchar(20) not null,
-   start_state varchar(20) not null,
-   start_zip smallint not null,
-   destination_addr varchar(20) not null,
-   destination_city varchar(20) not null,
-   destination_state varchar(20) not null,
-   destination_zip varchar(20) not null,
-   ride_status varchar(10) not null,
-   primary key (id)
- );
+--  create table ride (
+--    id bigserial not null,
+--    start_addr varchar(20) not null,
+--    start_city varchar(20) not null,
+--    start_state varchar(20) not null,
+--    start_zip smallint not null,
+--    destination_addr varchar(20) not null,
+--    destination_city varchar(20) not null,
+--    destination_state varchar(20) not null,
+--    destination_zip varchar(20) not null,
+--    ride_status varchar(10) not null,
+--    primary key (id)
+--  );
