bfaccdf6659626da6655810adb057164531c99cc
MarvinAmari <contact@marvinamari.com>
2018-05-08 22:22:59 -0400

    Implement JPARepository

--- src/main/java/org/aitesting/passenger/interfaces/PassengerRepository.java
+++ src/main/java/org/aitesting/passenger/interfaces/PassengerRepository.java
@@ -1,7 +1,10 @@
 package org.aitesting.passenger.interfaces;
 
 import org.aitesting.passenger.model.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PassengerRepository extends CrudRepository <Passenger, Long>{
+@Repository
+public interface PassengerRepository extends JpaRepository <Passenger, Long>{
 }
