a589dd2d2193f5292c80317399d287a3c71b9f48
MarvinAmari <contact@marvinamari.com>
2018-05-18 09:09:47 -0400

    Delete CRUD import statement

--- src/main/java/org/aitesting/passenger/interfaces/PassengerRepository.java
+++ src/main/java/org/aitesting/passenger/interfaces/PassengerRepository.java
@@ -2,7 +2,6 @@ package org.aitesting.passenger.interfaces;
 
 import org.aitesting.passenger.model.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
