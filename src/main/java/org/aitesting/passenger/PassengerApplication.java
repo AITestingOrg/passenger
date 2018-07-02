3d4ff6d26842ff41ebeb56f113b4f6ad088c6bbe
Richard Jimenez <Rjime061@fiu.edu>
2018-07-02 17:38:22 -0400

    feature/updatingSpringVersions (#3)
    Updated deprecated versions for springboot, and springcloud, and fixed naming conventions of discoveryservice.

--- src/main/java/org/aitesting/passenger/PassengerApplication.java
+++ src/main/java/org/aitesting/passenger/PassengerApplication.java
@@ -2,8 +2,9 @@ package org.aitesting.passenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PassengerApplication {
 	public static void main(String[] args) {
